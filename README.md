4396146e34f12a14cf77dd5860a35c11436c12c7
hkailee <leehongkai@gmail.com>
2020-11-18 21:35:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# C3.ai Therapeutic Agents/Drugs Mechanism of Action (MOA) and Performance Evaluation
+
+## Strategies covered
+1.
+
+## Folder structure
+    .
+    ├── Data/                # Contains data used in this study
+          └── figures/       # Contains figures generated in this study
+    ├── WorkBook.ipyn        # Working notebook with codes used in this study
+    ├── c3aidatalake.py      # A helper file shared across projects and quizzes
+    ├── utils.py             # A helper utility file used in this project
+    └── requirements.txt     # Common packages used for the packages
+
+## No Data
+No data available due to licensing issue
+
+## Source
+1. 
---
+++ utils.py
@@ -0,0 +1,71 @@
+import re
+from collections import Counter
+import plotly.graph_objects as go
+from tqdm import tqdm
+
+
+def preprocess(text):
+
+    # Replace punctuation with tokens so we can use them in our model
+    text = text.lower()
+    text = text.replace('.', ' <PERIOD> ')
+    text = text.replace(',', ' <COMMA> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace(';', ' <SEMICOLON> ')
+    text = text.replace('!', ' <EXCLAMATION_MARK> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('(', ' <LEFT_PAREN> ')
+    text = text.replace(')', ' <RIGHT_PAREN> ')
+    text = text.replace('--', ' <HYPHENS> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('\n', ' <NEW_LINE> ')
+    text = text.replace(':', ' <COLON> ')
+    words = text.split()
+    
+    # Remove all words with  5 or fewer occurences
+    word_counts = Counter(words)
+    trimmed_words = [word for word in tqdm(words, desc='Trimming words...', 
+                                unit=' words', position=0) if word_counts[word] > 5]
+    
+    return trimmed_words
+
+
+def create_lookup_tables(words):
+    """
+    Create lookup tables for vocabulary
+    :param words: Input list of words
+    :return: A tuple of dicts.  The first dict....
+    """
+    word_counts = Counter(words)
+    # sorting the words from most to least frequent in text occurrence
+    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    # create int_to_vocab dictionaries
+    int_to_vocab = {ii: word for ii, word in enumerate(sorted_vocab)}
+    vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
+
+    return vocab_to_int, int_to_vocab
+
+
+def make_edge(x, y, text, width):
+    
+    '''Creates a scatter trace for the edge between x's and y's with given width
+
+    Parameters
+    ----------
+    x    : a tuple of the endpoints' x-coordinates in the form, tuple([x0, x1, None])
+    
+    y    : a tuple of the endpoints' y-coordinates in the form, tuple([y0, y1, None])
+    
+    width: the width of the line
+
+    Returns
+    -------
+    An edge trace that goes between x0 and x1 with specified width.
+    '''
+    return  go.Scatter(x         = x,
+                       y         = y,
+                       line      = dict(width = width,
+                                   color = 'cornflowerblue'),
+                       hoverinfo = 'text',
+                       text      = ([text]),
+                       mode      = 'lines')
